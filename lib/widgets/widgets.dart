184d081fdafa55909e7123923c74f2c3a50b650c
mitegab <104057957+mitegab@users.noreply.github.com>
2023-09-24 03:12:12 +0000

    Update dependencies in pubspec.yaml and others

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,9 @@
+export 'location_search_box.dart';
+export 'category_box.dart';
+export 'promo_box.dart';
+export 'restaurant_card.dart';
+export 'restaurant_tags.dart';
+export 'restaurant_info.dart';
+export 'food_search_box.dart';
+export 'custom_category_filter.dart';
+export 'custom_price_filter.dart';
