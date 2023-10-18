3de470c170e803bdfa5bf0d4f8151d46910a4991
mitegab <104057957+mitegab@users.noreply.github.com>
2023-10-18 22:09:26 +0000

    Improve app performance

---
+++ lib/models/models.dart
@@ -0,0 +1,11 @@
+export 'place_autocomplete_model.dart';
+export 'place_model.dart';
+export 'category_model.dart';
+export 'restaurant_model.dart';
+export 'basket_model.dart';
+export 'voucher_model.dart';
+export 'menu_item_model.dart';
+export 'delivery_time_model.dart';
+export 'filter_model.dart';
+export 'category_filter_model.dart';
+export 'price_filter_model.dart';
