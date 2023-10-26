26e4c0aa333bc828e6199adc2e2392f81bb8055e
mitegab <104057957+mitegab@users.noreply.github.com>
2023-10-26 12:34:55 +0000

    Update README.md

---
+++ lib/blocs/blocs.dart
@@ -0,0 +1,6 @@
+export 'autocomplete/autocomplete_bloc.dart';
+export 'basket/basket_bloc.dart';
+export 'filter/filter_bloc.dart';
+export 'location/location_bloc.dart';
+export 'restaurants/restaurants_bloc.dart';
+export 'voucher/voucher_bloc.dart';
