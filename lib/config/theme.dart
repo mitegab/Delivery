d7da79d4fa2280ec228b8ac40df0fcf69a6b2d2e
mitegab <104057957+mitegab@users.noreply.github.com>
2024-03-16 10:12:17 +0300

    Update theme.dart

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 
+// Function to define and return custom theme data
 ThemeData theme() {
   return ThemeData(
+    // Define scaffold background color
     scaffoldBackgroundColor: Color(0xFFF5F5F5),
+    // Define color scheme for the theme
     colorScheme: ColorScheme(
+      // Primary color and its variant
       primary: Color(0xFFFE3C5B),
       primaryVariant: Color(0xFFFE3C5B),
+      // Secondary color and its variant
       secondary: Color(0xFFe84545),
       secondaryVariant: Color(0xFFe84545),
+      // Background and surface colors
       background: Color(0xFFFFFFFF),
       surface: Color(0xFFFFFFFF),
-      error: Color(0x000000),
+      // Error color
+      error: Color(0x000000), // This seems to be fully transparent black
+      // Define brightness of the theme
       brightness: Brightness.light,
+      // Text colors for various elements
       onPrimary: Color(0xFFFFFFFF),
       onSecondary: Color(0xFFFFFFFF),
       onError: Color(0xFF2b2e4a),
       onBackground: Color(0xFF2b2e4a),
       onSurface: Color(0xFF2b2e4a),
     ),
+    // Define default font family for the theme
     fontFamily: 'Futura',
+    // Define text theme with different styles for various text elements
     textTheme: TextTheme(
+      // Styles for headlines of different sizes
       headline1: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.bold,
@@ -50,6 +62,7 @@ ThemeData theme() {
         fontWeight: FontWeight.normal,
         fontSize: 14,
       ),
+      // Styles for body text with different sizes
       bodyText1: TextStyle(
         color: Color(0xFF2b2e4a),
         fontWeight: FontWeight.normal,
